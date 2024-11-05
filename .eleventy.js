4302d24b56db1a61be0fc7090b52bf222b4d8eb7
Lars Olesen <lars@intraface.dk>
2024-11-05 18:34:48 +0100

    No generate title based on filename if no title

--- .eleventy.js
+++ .eleventy.js
@@ -99,6 +99,24 @@ module.exports = async function(eleventyConfig) {
         md.render(markdownString),
     );
 
+    // Create titles for posts without a title in frontmatter
+    eleventyConfig.addGlobalData("eleventyComputed.title", () => (data) => {
+      // If property is explicitly set, use that
+      if (data.title) {
+        return data.title;
+      }
+
+      let slug = data.page.fileSlug;
+      let words = slug.split('-');
+
+      for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+      }
+
+      return words.join(' ');
+    });
+
     // Create computed excerpts per page
     eleventyConfig.addGlobalData("eleventyComputed.excerpt", () => (data) => {
       // If property is explicitly set, use that
