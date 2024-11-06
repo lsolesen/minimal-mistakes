a055220c381e401ab3ea14eea394f6e640535217
Lars Olesen <lars@intraface.dk>
2024-11-06 12:57:09 +0100

    Added footnote and a bit work on other stuff

--- .eleventy.js
+++ .eleventy.js
@@ -22,6 +22,7 @@ const markdownIt = require("markdown-it");
 const markdownItAttrs = require('markdown-it-attrs');
 const markdownItAnchor = require('markdown-it-anchor');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownItFootnote = require("markdown-it-footnote");
 
 let markdownitOptions = {
   html: true,
@@ -37,26 +38,27 @@ md.use(markdownItAttrs, {
   allowedAttributes: ["class", "rel"]  // empty array = all attributes are allowed
 });
 md.use(markdownItAnchor);
+md.use(markdownItFootnote);
 
 // Allow for data files to be in yaml
 const yaml = require("js-yaml");
 
+module.exports = async function (eleventyConfig) {
 
-module.exports = async function(eleventyConfig) {
+  // Return the keys used in an object
+  eleventyConfig.addFilter("getKeys", target => {
+    return Object.keys(target);
+  });
 
-	// Return the keys used in an object
-	eleventyConfig.addFilter("getKeys", target => {
-		return Object.keys(target);
-	});
-
-	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "posts"].indexOf(tag) === -1);
-	});
+  eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "posts"].indexOf(tag) === -1);
+  });
 
   // Does not work with categories
-	eleventyConfig.addFilter("filterCategoriesList", function filterCategoriesList(categories) {
-		return (categories || []).filter(category => ["all", "posts"].indexOf(category) === -1);
-	});
+  // TODO Fix that we can have a list with categories
+  eleventyConfig.addFilter("filterCategoriesList", function filterCategoriesList(categories) {
+    return (categories || []).filter(category => ["all", "posts"].indexOf(category) === -1);
+  });
 
   // https://saadbess.com/blog/creating-a-content-recommendation-plugin-in-11ty/
   eleventyConfig.addCollection("relatedPosts", function (collection) {
@@ -86,159 +88,192 @@ module.exports = async function(eleventyConfig) {
       });
   });
 
+  // Make it possible to have the site served in a sub directory
   const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
-	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
+  // Automatically improve images
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     // which file extensions to process
     extensions: 'html',
     // optional, output image formats
-    formats: ['jpg', 'webp'],
+    formats: ['jpg', 'png', 'webp'],
     // optional, output image widths
     widths: ['auto', 400, 800],
     // optional, attributes assigned on <img> override these values.
     defaultAttributes: {
-        loading: 'lazy',
-        sizes: '100vw',
-        decoding: 'async',
+      loading: 'lazy',
+      sizes: '100vw',
+      decoding: 'async',
     },
   });
 
-    // TODO Missing copy button
-    // Can be created using this
-    eleventyConfig.addPlugin(syntaxHighlight);
+  // Syntax highlighting with prism
+  // TODO Missing copy button
+  eleventyConfig.addPlugin(syntaxHighlight);
 
-    // Collections
-    // TODO ADD ALL COLLECTIONS
-    eleventyConfig.addCollection('posts', getPosts);
-    eleventyConfig.addCollection('docs', getDocs);
-    eleventyConfig.addCollection('pets', getPets);
-    eleventyConfig.addCollection('recipes', getRecipes);
-    eleventyConfig.addCollection('portfolio', getPortfolio);
+  // Collections
+  eleventyConfig.addCollection('posts', getPosts);
+  eleventyConfig.addCollection('docs', getDocs);
+  eleventyConfig.addCollection('pets', getPets);
+  eleventyConfig.addCollection('recipes', getRecipes);
+  eleventyConfig.addCollection('portfolio', getPortfolio);
 
-    // Pass through
-    eleventyConfig.addPassthroughCopy("assets/css");
-    eleventyConfig.addPassthroughCopy("assets/js");
-    eleventyConfig.addPassthroughCopy("assets/images/");
-    eleventyConfig.addPassthroughCopy({"docs/assets/images": "assets/images"});
+  // Pass through
+  eleventyConfig.addPassthroughCopy("assets/css");
+  eleventyConfig.addPassthroughCopy("assets/js");
+  eleventyConfig.addPassthroughCopy("assets/images/");
+  eleventyConfig.addPassthroughCopy({ "docs/assets/images": "assets/images" });
 
-    // Setup shortcodes and filters
-    eleventyConfig.setLibrary("md", md);
-    eleventyConfig.addFilter("markdownify", (markdownString) =>
-        md.render(markdownString),
-    );
+  // Setup shortcodes and filters
+  eleventyConfig.setLibrary("md", md);
+  eleventyConfig.addFilter("markdownify", (markdownString) =>
+    md.render(markdownString),
+  );
+  
+  // Create titles for posts without a title in frontmatter
+  eleventyConfig.addGlobalData("eleventyComputed.title", () => (data) => {
+    // If property is explicitly set, use that
+    if (data.title) {
+      return data.title;
+    }
 
-    // Create titles for posts without a title in frontmatter
-    eleventyConfig.addGlobalData("eleventyComputed.title", () => (data) => {
-      // If property is explicitly set, use that
-      if (data.title) {
-        return data.title;
-      }
+    let slug = data.page.fileSlug;
+    let words = slug.split('-');
 
-      let slug = data.page.fileSlug;
-      let words = slug.split('-');
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+      words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+    }
 
-      for (let i = 0; i < words.length; i++) {
-        let word = words[i];
-        words[i] = word.charAt(0).toUpperCase() + word.slice(1);
-      }
+    return words.join(' ');
+  });
 
-      return words.join(' ');
-    });
+  // Create computed excerpts per page if none has been explicitly set
+  eleventyConfig.addGlobalData("eleventyComputed.excerpt", () => (data) => {
+    // If property is explicitly set, use that
+    if (data.excerpt) {
+      return data.excerpt;
+    }
 
-    // Create computed excerpts per page
-    eleventyConfig.addGlobalData("eleventyComputed.excerpt", () => (data) => {
-      // If property is explicitly set, use that
-      if (data.excerpt) {
-        return data.excerpt;
-      }
+    // Grab raw page content
+    let content = data.page.rawInput;
 
-      // Grab raw page content
-      let content = data.page.rawInput;
+    // If template uses Markdown, render it
+    if (data.page.templateSyntax.includes('md')) {
+      content = md.render(content);
+    }
 
-      // If template uses Markdown, render it
-      if (data.page.templateSyntax.includes('md')) {
-        content = md.render(content);
-      }
+    // Vanilla paragraphs ending in period, question or exclamation
+    const matches = content.match(/<p>(.+[\.\?\!])<\/p>/);
 
-      // Vanilla paragraphs ending in period, question or exclamation
-      const matches = content.match(/<p>(.+[\.\?\!])<\/p>/);
+    // If found, return content
+    if (matches) {
+      return matches[1];
+    }
 
-      // If found, return content
-      if (matches) {
-        return matches[1];
-      }
+    return null;
+  });
 
-      return null;
-    });
-
-    // TODO FIX SHORTCODE TO REMOVE COLLECTION BASED STUFF
-    eleventyConfig.addFilter("absolute_url", (url) =>
-      url,
-    );
-
-    // TODO FIX SHORTCODE TO REMOVE COLLECTION BASED STUFF
-    eleventyConfig.addFilter("relative_url", (url) =>
-      url,
-    );
-
-    // @source https://24ways.org/2018/turn-jekyll-up-to-eleventy/
-    // {{ array | where: key,value }}
-    /*
-    eleventyConfig.addFilter('where', function (array, key, value) {
-      return array.filter(item => {
-        const keys = key.split('.');
-        const reducedKey = keys.reduce((object, key) => {
-          return object[key];
-        }, item);
-
-        return (reducedKey === value ? item : false);
-      });
-    });
-    */
-
-    // TODO FIX SHORTCODE TO REMOVE COLLECTION BASED STUFF
-    eleventyConfig.addShortcode("post_url", (url) => {
+  // absolute_url is deprecated - hardcode to your liking
+  // @deprecated
+  eleventyConfig.addFilter("absolute_url", (url) => {
       return url;
-    });
+    }
+  );
 
-    // TODO FIX SHORTCODE TO REMOVE COLLECTION BASED STUFF
-    eleventyConfig.addShortcode("link", (url) => {
-      return url;  
-    });
+  // Relative url is deprecated
+  // @deprecated
+  eleventyConfig.addFilter("relative_url", (url) => {
+      return url;
+    }
+  );
 
-    // @source https://24ways.org/2018/turn-jekyll-up-to-eleventy/
-    // TODO: Might be better to turn off in the future, but for now this makes i way easier
-    eleventyConfig.setLiquidOptions({
-      jekyllInclude: true, // allow to use jekyll style include
-      extname: ".liquid", // Use .liquid if not specified
-      dynamicPartials: false, // allow to use feature_row without quotes
-      //trimTagLeft: true,
-      //trimTagRight: false,
-      //trimOutputRight: true,
-      //trimOutputLeft: false
-    });
+  /**
+   * Define a post_url Liquid tag for cross-referencing
+   * 
+   * Original creator: https://rusingh.com/articles/2020/04/24/implement-jekyll-post-url-tag-11ty-shortcode/
+   * Adapted by me to work with filename instead of slug.
+   * 
+   * @param {*} collection 
+   * @param {*} filename 
+   * @returns 
+   */
+  eleventyConfig.addShortcode("post_url", (collection, filename) => {
+    if (collection.length < 1) {
+      throw "Collection appears to be empty";
+    }
+    if (!Array.isArray(collection)) {
+      throw "Collection is an invalid type - it must be an array!";
+    }
+    if (typeof filename !== "string") {
+      throw "Filename is an invalid type - it must be a string!";
+    }
+    const found = collection.find(p => p.template.inputPath.indexOf(filename) > -1);
+    if (found === 0 || found === undefined) {
+      // When nothing was found, throw an error to break the build.
+      // Broken links should not be allowed!
+      throw new Error(`File ${this.page.inputPath} wants to link to ${filename}, but it does not exist.`);
+    } else {
+      return found.url;
+    }
+  });
 
-    eleventyConfig.addDataExtension("yaml, yml", (contents) => yaml.load(contents));
-    
-    // eleventyConfig.addLayoutAlias('default', '_layouts/default.html')
-    // eleventyConfig.addLayoutAlias('single', '_layouts/single.liquid')
-    // eleventyConfig.addLayoutAlias('archive', 'archive.html')
+  /**
+   * Define a post_url Liquid tag for cross-referencing
+   * 
+   * Original creator: https://rusingh.com/articles/2020/04/24/implement-jekyll-post-url-tag-11ty-shortcode/
+   * Adapted by me to work with filename instead of slug.
+   * 
+   * @param {*} collection 
+   * @param {*} filename 
+   * @returns 
+   */
+  eleventyConfig.addShortcode("link", (collection, filename) => {
+    if (collection.length < 1) {
+      throw "Collection appears to be empty";
+    }
+    if (!Array.isArray(collection)) {
+      throw "Collection is an invalid type - it must be an array!";
+    }
+    if (typeof filename !== "string") {
+      throw "Filename is an invalid type - it must be a string!";
+    }
+    const found = collection.find(p => p.template.inputPath.indexOf(filename) > -1);
+    if (found === 0 || found === undefined) {
+      // When nothing was found, throw an error to break the build.
+      // Broken links should not be allowed!
+      throw new Error(`File ${this.page.inputPath} wants to link to ${filename}, but it does not exist.`);
+    } else {
+      return found.url;
+    }
+  });
 
-    // Configure excerpt
-    eleventyConfig.setFrontMatterParsingOptions({
-      excerpt: true
-    });
+  // @source https://24ways.org/2018/turn-jekyll-up-to-eleventy/
+  // TODO: Might be better to turn off in the future, but for now this makes i way easier
+  eleventyConfig.setLiquidOptions({
+    jekyllInclude: true, // allow to use jekyll style include
+    extname: ".liquid", // Use .liquid if not specified
+    dynamicPartials: false // allow to use feature_row without quotes
+  });
 
-    return {
-      dir: {
-        input: "docs",
-        includes: "../_includes",
-        layouts: "../_layouts",
-        htmlTemplateEngine: "liquid",
-        dataTemplateEngine: "html",
-        output: "_site",
-        pathPrefix: "/minimal-mistakes"
-      },
-    };
+  // Make it possible to use yaml as settings
+  eleventyConfig.addDataExtension("yaml, yml", (contents) => yaml.load(contents));
+
+  // Configure excerpt
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true
+  });
+
+  return {
+    dir: {
+      input: "docs",
+      includes: "../_includes",
+      layouts: "../_layouts",
+      htmlTemplateEngine: "liquid",
+      dataTemplateEngine: "html",
+      output: "_site",
+      pathPrefix: "/minimal-mistakes"
+    },
+  };
 };
--- docs/_pages/about.md
+++ docs/_pages/about.md
@@ -40,24 +40,15 @@ Minimal Mistakes is a flexible two-column Jekyll theme. Perfect for hosting your
 
 | Name                                        | Description                                           |
 | ------------------------------------------- | ----------------------------------------------------- |
-| [Post with Header Image][header-image-post] | A post with a large header image. |
-| [HTML Tags and Formatting Post][html-tags-post] | A variety of common markup showing how the theme styles them. |
-| [Syntax Highlighting Post][syntax-post] | Post displaying highlighted code. |
-| [Post with a Gallery][gallery-post] | A post showing several images wrapped in `<figure>` elements. |
-| [Sample Collection Page][sample-collection] | Single page from a collection. |
-| [Categories Archive][categories-archive] | Posts grouped by category. |
-| [Tags Archive][tags-archive] | Posts grouped by tag. |
+| [Post with Header Image]({% post_url collections.posts, "2012-03-15-layout-header-image-text-readability.md" %}) | A post with a large header image. |
+| [HTML Tags and Formatting Post]({% post_url collections.posts, "2013-01-11-markup-html-tags-and-formatting" %}) | A variety of common markup showing how the theme styles them. |
+| [Syntax Highlighting Post]({% post_url collections.posts, "2013-08-16-markup-syntax-highlighting" %}) | Post displaying highlighted code. |
+| [Post with a Gallery]({% post_url collections.posts, "2010-09-09-post-gallery" %}) | A post showing several images wrapped in `<figure>` elements. |
+| [Sample Collection Page]({{ "/recipes/chocolate-chip-cookies/" | relative_url }}) | Single page from a collection. |
+| [Categories Archive]({{ "/categories/" | relative_url }}) | Posts grouped by category. |
+| [Tags Archive]({{ "/tags/" | relative_url }}) | Posts grouped by tag. |
 
-For even more demo pages check the [posts archive][year-archive].
-
-[header-image-post]: {{ "" | relative_url }}{% post_url "2012-03-15-layout-header-image-text-readability" %}
-[gallery-post]: {{ "" | relative_url }}{% post_url "2010-09-09-post-gallery" %}
-[html-tags-post]: {{ "" | relative_url }}{% post_url "2013-01-11-markup-html-tags-and-formatting" %}
-[syntax-post]: {{ "" | relative_url }}{% post_url "2013-08-16-markup-syntax-highlighting" %}
-[sample-collection]: {{ "/recipes/chocolate-chip-cookies/" | relative_url }}
-[categories-archive]: {{ "/categories/" | relative_url }}
-[tags-archive]: {{ "/tags/" | relative_url }}
-[year-archive]: {{ "/year-archive/" | relative_url }}
+For even more demo pages check the [posts archive]({{ "/year-archive/" | relative_url }}).
 
 ---
 
