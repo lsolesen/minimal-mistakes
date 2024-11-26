8233f713889891a7bbe71ad6ae6f64460ae34660
Lars Olesen <lars@intraface.dk>
2024-11-26 20:29:03 +0100

    Only create png if they are already present

--- _config/mainConfig.mjs
+++ _config/mainConfig.mjs
@@ -67,7 +67,7 @@ export default function (eleventyConfig) {
     // which file extensions to process
     extensions: 'html',
     // optional, output image formats
-    formats: ['jpg', 'png', 'webp'],
+    formats: ['auto', 'jpg', 'webp'],
     // optional, output image widths
     widths: ['auto', 400, 800],
     // optional, attributes assigned on <img> override these values.
